9dc618607ddb037f8d39618356a8c78cc7e1f9dd
Tim Chao <tim.chao@ingics.com>
2019-10-17 05:51:00 +0000

    add setup.py and __init__.py

---
+++ igsparser/__init__.py
@@ -0,0 +1,5 @@
+from message_parser import MessageParser
+from payload_parser import PayloadParser
+
+__all__ = [ 'MessageParser', 'PayloadParser' ]
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "ingics_message_parser",
+        version = "0.1.0",
+        license = "MIT Licence",
+        packages = find_packages(),
+        zip_safe=False
+)
+
