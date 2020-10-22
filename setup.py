189be71c4a61a6d068d3181959226e11a13e8499
Tim Chao <tim.chao@ingics.com>
2020-10-22 17:19:37 +0800

    chore: update version number, cause previous commit change the data format
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "ingics_message_parser",
-        version = "0.2.0",
+        version = "0.2.1",
         license = "MIT Licence",
         packages = find_packages(),
         zip_safe=False
