3d68f497540fa5a2b6a195556ab8d51a18e43944
Tim Chao <tim.chao@ingics.com>
2021-05-20 15:40:54 +0800

    chore: clean up code for flake8 verification
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- bin/parse_message.py
+++ bin/parse_message.py
@@ -4,11 +4,15 @@ import sys
 import pprint
 from igsparser import MessageParser as parser
 
+
 def cb(data, index):
     pprint.pprint(vars(data))
+
 
 if len(sys.argv) > 1:
     parser.parse(sys.argv[1], cb)
 else:
     print('Usage: {} <iGS Message>'.format(sys.argv[0]))
-    print('Example: {} \'$GPRP,F973D9D36662,E7DAE08E6FC3,-87,02010612FF590080BC330100FFFFFFFF000004000000\''.format(sys.argv[0]))
+    print('Example: {} \'{}\''.format(
+        sys.argv[0],
+        '$GPRP,F973D9D36662,E7DAE08E6FC3,-87,02010612FF590080BC330100FFFFFFFF000004000000'))
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "ingics_message_parser",
-        version = "0.3.0",
-        license = "MIT Licence",
-        packages = find_packages(),
-        zip_safe = False
+        name="ingics_message_parser",
+        version="0.3.0",
+        license="MIT Licence",
+        packages=find_packages(),
+        zip_safe=False
 )
