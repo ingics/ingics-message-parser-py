c764a4793752fba1b53cea52252d3e227c0dcf9a
Tim Chao <tim.chao@ingics.com>
2025-04-30 11:41:06 +0800

    chore: move to pyproject toml
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-        name="ingics_message_parser",
-        version="0.5.1",
-        license="MIT License",
-        packages=find_packages(),
-        zip_safe=False
-)
